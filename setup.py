e31d17938687da25ac480d771b487acabb405cd5
strayedelectron <strayedelectron@gmail.com>
2022-12-09 15:21:38 +0100

    setup, requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='dab-modulation-optimizer',
+	version='',
+    python_requires='>=3.8',
+    install_requires=['numpy>=1.19.5',],
+	packages=[''],
+	url='',
+    license="GNU General Public License v3",
+	author='strayedelectron',
+	author_email='',
+	description=''
+)
