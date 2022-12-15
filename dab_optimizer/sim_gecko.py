79e19c9023ffa5541fef139c5a864811cc84286f
strayedelectron <strayedelectron@gmail.com>
2022-12-15 22:37:53 +0100

    some sim code improvements

--- dab_optimizer/sim_gecko.py
+++ dab_optimizer/sim_gecko.py
@@ -28,34 +28,27 @@ def start_sim(DAB: ds.DAB_Specification, mvvp_phi, mvvp_tau1, mvvp_tau2):
 	print(mvvp_iLs.shape)
 	mvvp_S11_p_sw = np.full_like(DAB.mesh_V1, np.nan)
 
-	# for idx in np.ndindex(array.shape):
-	# ...     print(idx, end=' ')
-	# (0, 0, 0) (0, 0, 1)
-
-	# ugly but for testing until np.array is implemented
-	# for V1, V2, P in itertools.product(range(DAB.V1_min, DAB.V1_max + 1, 100),
-	# 								   range(DAB.V2_min, DAB.V2_max + 1, 60),
-	# 								   range(DAB.P_min, DAB.P_max + 1, 200)):
-
 	for vec_vvp in np.ndindex(mvvp_iLs.shape):
 		#print(vec_vvp, mvvp_phi[vec_vvp], mvvp_tau1[vec_vvp], mvvp_tau2[vec_vvp], sep='\n')
 
 		# set simulation parameters and convert tau to inverse-tau for Gecko
 		sim_params = {
 			#TODO find a way to do this with sparse arrays
-			'v_dc1': DAB.mesh_V1[vec_vvp],
-			'v_dc2': DAB.mesh_V2[vec_vvp],
-			'phi': mvvp_phi[vec_vvp] / np.pi * 180,
-			'tau1_inv': (np.pi - mvvp_tau1[vec_vvp]) / np.pi * 180,
-			'tau2_inv': (np.pi - mvvp_tau2[vec_vvp]) / np.pi * 180
+			'v_dc1': DAB.mesh_V1[vec_vvp].item(),
+			'v_dc2': DAB.mesh_V2[vec_vvp].item(),
+			'phi': mvvp_phi[vec_vvp].item() / np.pi * 180,
+			'tau1_inv': (np.pi - mvvp_tau1[vec_vvp].item()) / np.pi * 180,
+			'tau2_inv': (np.pi - mvvp_tau2[vec_vvp].item()) / np.pi * 180
 		}
 		print(sim_params)
+		#print("phi: ", type(sim_params['phi']))
 
 		# start simulation for this operation point
 		#TODO optimize for multithreading, maybe multiple Gecko instances needed
 		dab_converter.set_global_parameters(sim_params)
 		#TODO time settings should be variable
 		dab_converter.run_simulation(timestep=100e-12, simtime=15e-6, timestep_pre=50e-9, simtime_pre=10e-3)
+		#dab_converter.run_simulation(timestep=100e-12, simtime=15e-6)
 		values_mean = dab_converter.get_values(
 			nodes=['p_dc1', 'S11_p_sw', 'S11_p_cond', 'S12_p_sw', 'S12_p_cond'],
 			operations=['mean']
@@ -65,7 +58,8 @@ def start_sim(DAB: ds.DAB_Specification, mvvp_phi, mvvp_tau1, mvvp_tau2):
 			operations=['rms']
 		)
 		power_deviation = DAB.mesh_P[vec_vvp].item() and values_mean['mean']['p_dc1'] / DAB.mesh_P[vec_vvp].item()
-		print(power_deviation)
+		print("power_target", DAB.mesh_P[vec_vvp].item())
+		print("power_deviation", power_deviation)
 
 		# save simulation results in array
 		mvvp_iLs[vec_vvp] = values_rms['rms']['i_Ls']
@@ -105,23 +99,6 @@ def start_sim_dict(DAB: ds.DAB_Specification, phi, tau1, tau2):
 			operations=['rms']
 		)
 		power_deviation = values_mean['mean']['p_dc1'] / P
-
-
-
-
-
-	# params = dab_converter.get_global_parameters(['phi', 'tau1_inv', 'tau2_inv'])
-	# print(params)
-	# params = {'phi': 80.0, 'tau1_inv': 40.0, 'tau2_inv': 66.0}
-	# dab_converter.set_global_parameters(params)
-	#
-	# dab_converter.run_simulation(timestep=50e-9, simtime=15e-6)
-	#
-	# dab_converter.get_scope_data(node_names=['v1', 'v2_1', 'i_HF1', 'S11_p_sw'], file_name='test')
-	#
-	# values = dab_converter.get_values(nodes=['v1', 'v2_1', 'i_HF1', 'S11_p_sw'], operations=['mean', 'rms'], range_start_stop=[10e-6, 15e-6])
-	# print(values)
-
 
 
 
