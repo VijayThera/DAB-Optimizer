75935de43d45e2e16a3766513de39d250a08e1f3
strayedelectron <strayedelectron@gmail.com>
2023-01-17 01:25:21 +0100

    opt: better default plotting, some minor changes

--- dab_optimizer/dab_opt.py
+++ dab_optimizer/dab_opt.py
@@ -240,23 +240,61 @@ def dab_sim_save():
 
     # Simulation
     dab_sim = sim_gecko.SimGecko()
-    d_sim = dab_sim.start_sim_threads(dab_results.mesh_V1,
+    # d_sim = dab_sim.start_sim_threads(dab_results.mesh_V1,
+    #                                   dab_results.mesh_V2,
+    #                                   dab_results.mod_cpm_phi,
+    #                                   dab_results.mod_cpm_tau1,
+    #                                   dab_results.mod_cpm_tau2,
+    #                                   simfilepath, timestep, simtime)
+
+    d_sim = dab_sim.start_sim_multi(dab_results.mesh_V1,
                                 dab_results.mesh_V2,
                                 dab_results.mod_cpm_phi,
                                 dab_results.mod_cpm_tau1,
                                 dab_results.mod_cpm_tau2,
                                 simfilepath, timestep, simtime)
 
-    # d_sim = dab_sim.start_sim_multi(dab_results.mesh_V1,
-    #                             dab_results.mesh_V2,
-    #                             dab_results.mod_cpm_phi,
-    #                             dab_results.mod_cpm_tau1,
-    #                             dab_results.mod_cpm_tau2,
-    #                             simfilepath, timestep, simtime)
-
     # Unpack the results
     for k, v in d_sim.items():
         dab_results['sim_' + k] = v
+
+    debug("sim_i_Ls: \n", dab_results.sim_i_Ls)
+    debug("sim_S11_p_sw: \n", dab_results.sim_S11_p_sw)
+
+    # Plotting
+    info("\nStart Plotting\n")
+    plt_dab = plot_dab.Plot_DAB()
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Overview')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.mod_cpm_phi[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][0],
+                             xlabel='P / W', ylabel='U2 / V', title='phi in rad')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_i_Ls[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][1],
+                             xlabel='P / W', ylabel='U2 / V', title='i_Ls / A')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_S11_p_sw[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][2],
+                             xlabel='P / W', ylabel='U2 / V', title='S11_p_sw / W')
+
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Power')
+    plt_dab.plot_3by1(plt_dab.figs_axes[-1],
+                      dab_results.mesh_P[:, 1, :],
+                      dab_results.mesh_V2[:, 1, :],
+                      dab_results.sim_p_dc1[:, 1, :],
+                      dab_results.sim_S11_p_sw[:, 1, :],
+                      dab_results.sim_S11_p_cond[:, 1, :],
+                      'P / W',
+                      'U2 / V',
+                      'p_dc1',
+                      'S11_p_sw',
+                      'S11_p_cond')
+
+    plt_dab.show()
 
     # Calc power deviation from expected power target
     # power_deviation = mesh_P[vec_vvp].item() and values_mean['mean']['p_dc1'] / mesh_P[vec_vvp].item()
@@ -278,15 +316,15 @@ def test_dab():
     dab_specs.V1_nom = 700
     dab_specs.V1_min = 600
     dab_specs.V1_max = 800
-    dab_specs.V1_step = 3
+    dab_specs.V1_step = 2
     dab_specs.V2_nom = 235
     dab_specs.V2_min = 175
     dab_specs.V2_max = 295
-    dab_specs.V2_step = 3
+    dab_specs.V2_step = 2
     dab_specs.P_min = 400
     dab_specs.P_max = 2200
     dab_specs.P_nom = 2000
-    dab_specs.P_step = 3
+    dab_specs.P_step = 2
     dab_specs.n = 2.99
     dab_specs.L_s = 84e-6
     dab_specs.L_m = 599e-6
@@ -308,11 +346,11 @@ def test_dab():
     # Modulation Calculation
     # TODO how to name the arrays according to some kind of given pattern?
     dab_results.mod_cpm_phi, dab_results.mod_cpm_tau1, dab_results.mod_cpm_tau2 = mod_cpm.calc_modulation(dab_specs.n,
-                                                                                              dab_specs.L_s,
-                                                                                              dab_specs.fs_nom,
-                                                                                              dab_results.mesh_V1,
-                                                                                              dab_results.mesh_V2,
-                                                                                              dab_results.mesh_P)
+                                                                                                          dab_specs.L_s,
+                                                                                                          dab_specs.fs_nom,
+                                                                                                          dab_results.mesh_V1,
+                                                                                                          dab_results.mesh_V2,
+                                                                                                          dab_results.mesh_P)
 
     # TODO where to save??? spec only float...
     simfilepath = '../circuits/DAB_MOSFET_Modulation_Lm_nlC.ipes'
@@ -335,27 +373,43 @@ def test_dab():
     debug("sim_S11_p_sw: \n", dab_results.sim_S11_p_sw)
 
     # Plotting
-    pw = plotWindow()
-    fig = plot_dab.plot_modulation(dab_results.mesh_V2,
-                                   dab_results.mesh_P,
-                                   dab_results.mod_cpm_phi,
-                                   dab_results.mod_cpm_tau1,
-                                   dab_results.mod_cpm_tau2)
-    pw.addPlot("DAB Modulation Angles", fig)
-    fig = plot_dab.plot_rms_current(dab_results.mesh_V2,
-                                    dab_results.mesh_P,
-                                    dab_results.sim_i_Ls)
-    pw.addPlot("i_Ls", fig)
-    fig = plot_dab.plot_rms_current(dab_results.mesh_V2,
-                                    dab_results.mesh_P,
-                                    dab_results.sim_S11_p_sw)
-    pw.addPlot("S11 p_sw", fig)
-    # plot_dab.show_plot()
-    pw.show()
+    info("\nStart Plotting\n")
+    plt_dab = plot_dab.Plot_DAB()
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Overview')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.mod_cpm_phi[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][0],
+                             xlabel='P / W', ylabel='U2 / V', title='phi in rad')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_i_Ls[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][1],
+                             xlabel='P / W', ylabel='U2 / V', title='i_Ls / A')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_S11_p_sw[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][2],
+                             xlabel='P / W', ylabel='U2 / V', title='S11_p_sw / W')
+
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Power')
+    plt_dab.plot_3by1(plt_dab.figs_axes[-1],
+                      dab_results.mesh_P[:, 1, :],
+                      dab_results.mesh_V2[:, 1, :],
+                      dab_results.sim_p_dc1[:, 1, :],
+                      dab_results.sim_S11_p_sw[:, 1, :],
+                      dab_results.sim_S11_p_cond[:, 1, :],
+                      'P / W',
+                      'U2 / V',
+                      'p_dc1',
+                      'S11_p_sw',
+                      'S11_p_cond')
+
+    plt_dab.show()
 
     # Saving
-    # save_to_file(dab_specs, dab_results, name='test-save', comment='This is a saving test with random data!')
-    save_to_file(dab_specs, dab_results, name='test-save', timestamp=False, comment='This is a saving test with random data!')
+    save_to_file(dab_specs, dab_results, name='test-save', comment='This is a saving test with random data!')
+    # save_to_file(dab_specs, dab_results, name='test-save', timestamp=False, comment='This is a saving test with random data!')
 
     # Loading
     # dab_specs_loaded, dab_results_loaded = load_from_file('test-save.npz')
@@ -375,19 +429,41 @@ def test_dab():
 
 def test_plot():
     # Loading
-    dab_specs, dab_results = load_from_file('test-save.npz')
-    # dab_specs.pprint()
+    dab_specs, dab_results = load_from_file('test-cpm-save.npz')
+    dab_specs.pprint()
     # dab_results.pprint()
 
     info("\nStart Plotting\n")
     plt_dab = plot_dab.Plot_DAB()
-    plt_dab.new_fig(1, 3)
-    plt_dab.plot_modulation(plt_dab.figs_axes[-1],
-                            dab_results.mesh_P[:, 1, :],
-                            dab_results.mesh_V2[:, 1, :],
-                            dab_results.mod_cpm_phi[:, 1, :],
-                            dab_results.mod_cpm_tau1[:, 1, :],
-                            dab_results.mod_cpm_tau2[:, 1, :])
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Overview')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.mod_cpm_phi[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][0],
+                             xlabel='P / W', ylabel='U2 / V', title='phi in rad')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_i_Ls[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][1],
+                             xlabel='P / W', ylabel='U2 / V', title='i_Ls / A')
+    plt_dab.subplot_contourf(dab_results.mesh_P[:, 1, :],
+                             dab_results.mesh_V2[:, 1, :],
+                             dab_results.sim_S11_p_sw[:, 1, :],
+                             ax=plt_dab.figs_axes[-1][1][2],
+                             xlabel='P / W', ylabel='U2 / V', title='S11_p_sw / W')
+
+    plt_dab.new_fig(nrows=1, ncols=3, tab_title='CPM Power')
+    plt_dab.plot_3by1(plt_dab.figs_axes[-1],
+                      dab_results.mesh_P[:, 1, :],
+                      dab_results.mesh_V2[:, 1, :],
+                      dab_results.sim_p_dc1[:, 1, :],
+                      dab_results.sim_S11_p_sw[:, 1, :],
+                      dab_results.sim_S11_p_cond[:, 1, :],
+                      'P / W',
+                      'U2 / V',
+                      'p_dc1',
+                      'S11_p_sw',
+                      'S11_p_cond')
 
     plt_dab.new_fig(1, 3)
     plt_dab.plot_modulation(plt_dab.figs_axes[-1],
@@ -397,8 +473,8 @@ def test_plot():
                             dab_results.mod_cpm_tau1[:, 1, :],
                             dab_results.mod_cpm_tau2[:, 1, :])
 
-    # now redraw the first fig
-    plt_dab.plot_modulation(plt_dab.figs_axes[0],
+    # now redraw the previous fig
+    plt_dab.plot_modulation(plt_dab.figs_axes[-1],
                             dab_results.mesh_P[:, 1, :],
                             dab_results.mesh_V2[:, 1, :],
                             dab_results.mod_cpm_phi[:, 1, :],
@@ -415,11 +491,11 @@ if __name__ == '__main__':
     main_init()
 
     # Generate simulation data
-    # dab_sim_save()
+    dab_sim_save()
 
     # Test the DAB functions
-    test_dab()
+    # test_dab()
     # Test the Plot functions
-    test_plot()
+    # test_plot()
 
     # sys.exit(0)
