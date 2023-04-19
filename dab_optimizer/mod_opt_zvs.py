7089063ea43a284be5e0efd8e6657e7bf15244ec
strayedelectron <strayedelectron@gmail.com>
2023-04-20 00:04:07 +0200

    Start: OptZVS empty file

--- dab_optimizer/mod_opt_zvs.py
+++ dab_optimizer/mod_opt_zvs.py
@@ -22,21 +22,45 @@ import numpy as np
 import dab_datasets as ds
 from debug_tools import *
 
+# The dict keys this modulation will return
+MOD_KEYS = ['mod_optzvs_phi', 'mod_optzvs_tau1', 'mod_optzvs_tau2', 'mod_optzvs_mask_zvs', 'mod_optzvs_mask_m1n', 'mod_optzvs_mask_m1p', 'mod_optzvs_mask_m2']
+
 
 @timeit
-def calc_modulation(n, L_s, fs_nom, mesh_V1, mesh_V2, mesh_P):
-    # init 3d arrays
-    # mvvp_phi = np.zeros_like(mesh_V1)
-    # init these with pi because they are constant for CPM
-    mvvp_tau1 = np.full_like(mesh_V1, np.pi)
-    mvvp_tau2 = np.full_like(mesh_V1, np.pi)
+def calc_modulation(n, L_s, fs_nom, mesh_V1, mesh_V2, mesh_P) -> dict:
+    """
+    OptZVS (Optimal ZVS) Modulation calculation, which will return phi, tau1 and tau2
 
-    # Calculate phase shift difference from input to output bridge
-    # TODO maybe have to consider the case sqrt(<0). When does this happen?
-    # maybe like this: mln_phi[mln_phi < 0] = np.nan
-    mvvp_phi = np.pi / 2 * (1 - np.sqrt(1 - (8 * fs_nom * L_s * mesh_P) / (n * mesh_V1 * mesh_V2)))
+    :param n: Transformer turns ratio
+    :param L_s: DAB converter inductance.
+    :param fs_nom: Switching frequency
+    :param mesh_V1: input voltage (voltage on side 1)
+    :param mesh_V2: output voltage (voltage on side 2)
+    :param mesh_P: DAB power (assuming a lossless DAB)
+    :return: dict with phi, tau1, tau2, masks
+    """
 
-    return mvvp_phi, mvvp_tau1, mvvp_tau2
+
+
+
+
+
+
+    # Init return dict
+    da_mod_results = dict()
+    # Save the results in the dict
+    # da_mod_results[MOD_KEYS[0]] = phi
+    # Convert phi because the math from the paper uses Middle-Pulse alignment but we use First-Falling-Edge alignment!
+    da_mod_results[MOD_KEYS[0]] = phi
+    da_mod_results[MOD_KEYS[1]] = tau1
+    da_mod_results[MOD_KEYS[2]] = tau2
+    da_mod_results[MOD_KEYS[3]] = zvs
+    da_mod_results[MOD_KEYS[4]] = _m1p_mask
+    da_mod_results[MOD_KEYS[5]] = _m1n_mask
+    da_mod_results[MOD_KEYS[6]] = _m2_mask
+
+    return da_mod_results
+
 
 
 # ---------- MAIN ----------
@@ -47,15 +71,15 @@ if __name__ == '__main__':
     Dab_Specs.V1_nom = 700
     Dab_Specs.V1_min = 600
     Dab_Specs.V1_max = 800
-    Dab_Specs.V1_step = 3
+    Dab_Specs.V1_step = 21 * 3
     Dab_Specs.V2_nom = 235
     Dab_Specs.V2_min = 175
     Dab_Specs.V2_max = 295
-    Dab_Specs.V2_step = 4
+    Dab_Specs.V2_step = 25 * 3
     Dab_Specs.P_min = 400
     Dab_Specs.P_max = 2200
     Dab_Specs.P_nom = 2000
-    Dab_Specs.P_step = 5
+    Dab_Specs.P_step = 19 * 3
     Dab_Specs.n = 2.99
     Dab_Specs.L_s = 84e-6
     Dab_Specs.L_m = 599e-6
@@ -70,29 +94,43 @@ if __name__ == '__main__':
         Dab_Specs.P_min, Dab_Specs.P_max, Dab_Specs.P_step)
 
     # Modulation Calculation
-    Dab_Results.mod_phi, Dab_Results.mod_tau1, Dab_Results.mod_tau2 = calc_modulation(Dab_Specs.n,
-                                                                                      Dab_Specs.L_s,
-                                                                                      Dab_Specs.fs_nom,
-                                                                                      Dab_Results.mesh_V1,
-                                                                                      Dab_Results.mesh_V2,
-                                                                                      Dab_Results.mesh_P)
+    da_mod = calc_modulation(Dab_Specs.n,
+                             Dab_Specs.L_s,
+                             Dab_Specs.fs_nom,
+                             Dab_Results.mesh_V1,
+                             Dab_Results.mesh_V2,
+                             Dab_Results.mesh_P)
 
-    print("mod_phi:", Dab_Results.mod_phi, sep='\n')
-    print("mod_tau1:", Dab_Results.mod_tau1, sep='\n')
-    print("mod_tau2:", Dab_Results.mod_tau2, sep='\n')
-    print("mod_phi[0,0,0]", type(Dab_Results.mod_phi[0, 0, 0]))
-    print("mod_tau1[0,0,0]", type(Dab_Results.mod_tau1[0, 0, 0]))
-    print("mod_tau2[0,0,0]", type(Dab_Results.mod_tau2[0, 0, 0]))
+    # Unpack the results
+    Dab_Results.append_result_dict(da_mod)
 
     info("\nStart Plotting\n")
     import plot_dab
+
+    v1_middle = int(np.shape(Dab_Results.mesh_P)[1] / 2)
+
     Plot_Dab = plot_dab.Plot_DAB()
     # Plot all modulation angles
     Plot_Dab.new_fig(nrows=1, ncols=3, tab_title='DAB Modulation Angles')
     Plot_Dab.plot_modulation(Plot_Dab.figs_axes[-1],
-                      Dab_Results.mesh_P[:, 1, :],
-                      Dab_Results.mesh_V2[:, 1, :],
-                      Dab_Results.mod_phi[:, 1, :],
-                      Dab_Results.mod_tau1[:, 1, :],
-                      Dab_Results.mod_tau2[:, 1, :])
+                             Dab_Results.mesh_P[:, v1_middle, :],
+                             Dab_Results.mesh_V2[:, v1_middle, :],
+                             Dab_Results.mod_optzvs_phi[:, v1_middle, :],
+                             Dab_Results.mod_optzvs_tau1[:, v1_middle, :],
+                             Dab_Results.mod_optzvs_tau2[:, v1_middle, :],
+                             mask1=Dab_Results.mod_optzvs_mask_tcm[:, v1_middle, :],
+                             mask2=Dab_Results.mod_optzvs_mask_cpm[:, v1_middle, :])
+
+    # Plot animation for every V1 cross-section
+    # for v1 in range(0, np.shape(Dab_Results.mesh_P)[1] - 1):
+    #     print(v1)
+    #     Plot_Dab.plot_modulation(Plot_Dab.figs_axes[-1],
+    #                              Dab_Results.mesh_P[:, v1, :],
+    #                              Dab_Results.mesh_V2[:, v1, :],
+    #                              Dab_Results.mod_optzvs_phi[:, v1, :],
+    #                              Dab_Results.mod_optzvs_tau1[:, v1, :],
+    #                              Dab_Results.mod_optzvs_tau2[:, v1, :],
+    #                              mask1=Dab_Results.mod_optzvs_mask_tcm[:, v1, :],
+    #                              mask2=Dab_Results.mod_optzvs_mask_cpm[:, v1, :])
+
     Plot_Dab.show()
