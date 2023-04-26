30afdcc5f73a4f8cc24a79336c2eaff57ea73814
strayedelectron <strayedelectron@gmail.com>
2023-04-26 22:06:13 +0200

    Change: dab_datasets: Add a new all-in-one DAB_Data class. Save/Load funcs now in module.

--- dab_optimizer/dab_datasets.py
+++ dab_optimizer/dab_datasets.py
@@ -3,11 +3,224 @@
 # coding: utf-8
 # python >= 3.10
 
+import os
 import numpy as np
 from dotmap import DotMap
+import math
 
 from debug_tools import *
 
+
+class DAB_Data(DotMap):
+    """
+    Class to store the DAB specification, modulation and simulation results and some more data.
+    It contains only numpy arrays, you can only add those.
+    In fact everything is a numpy array even single int or float values!
+    It inherits from DotMap to provide dot-notation usage instead of regular dict access.
+    Make sure your key names start with one of the "_allowed_keys", if not you can not add the key.
+    Add a useful name string after the prefix from "_allowed_keys" to identify your results later.
+    """
+
+    _allowed_keys = ['_timestamp', '_comment', 'spec_', 'mesh_', 'mod_', 'sim_', 'coss_', 'qoss_']
+    _allowed_spec_keys = ['V1_nom', 'V1_min', 'V1_max', 'V1_step', 'V2_nom', 'V2_min', 'V2_max', 'V2_step', 'P_min',
+                          'P_max', 'P_nom', 'P_step', 'n', 'L_s', 'L_m', 'fs_nom']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialisation with an other Dict is not handled and type converted yet!
+        :param args:
+        :param kwargs:
+        """
+        if args or kwargs:
+            warning("Don't use this type of initialisation!")
+        # if kwargs:
+        #     d.update((k, float(v)) for k,v in self.__call_items(kwargs)
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, k, v):
+        # Only np.ndarray is allowed
+        if isinstance(v, np.ndarray):
+            # Check for allowed key names
+            if any(k.startswith(allowed_key) for allowed_key in (self._allowed_keys + self._allowed_spec_keys)):
+                super().__setitem__(k, v)
+            else:
+                warning('None of the _allowed_keys are used! Nothing added! Used key: ' + str(k))
+        else:
+            warning('Value is not an numpy ndarray! Nothing added! Used type: ' + str(type(v)))
+
+            # Check for allowed key names
+            if any(k.startswith(allowed_key) for allowed_key in (self._allowed_keys + self._allowed_spec_keys)):
+                super().__setitem__(k, np.asarray(v))
+            else:
+                warning('None of the _allowed_keys are used! Nothing added! Used key: ' + str(k))
+
+    def gen_meshes(self):
+        """
+        Generates the default meshgrids for V1, V2 and P.
+        Values for:
+        'V1_nom', 'V1_min', 'V1_max', 'V1_step',
+        'V2_nom', 'V2_min', 'V2_max', 'V2_step',
+        'P_min', 'P_max', 'P_nom', 'P_step'
+        must be set first!
+        """
+        self.mesh_V1, self.mesh_V2, self.mesh_P = np.meshgrid(np.linspace(self.V1_min, self.V1_max, int(self.V1_step)),
+                                                              np.linspace(self.V2_min, self.V2_max, int(self.V2_step)),
+                                                              np.linspace(self.P_min, self.P_max, int(self.P_step)),
+                                                              sparse=False)
+
+    def append_result_dict(self, result: dict, name_pre: str = '', name_post: str = ''):
+        # Unpack the results
+        for k, v in result.items():
+            self[name_pre + k + name_post] = v
+
+
+def save_to_file(dab: DAB_Data, directory=str(), name=str(), timestamp=True, comment=str()):
+    """
+    Save everything (except plots) in one file.
+    WARNING: Existing files will be overwritten!
+
+    File is ZIP compressed and contains several named np.ndarray objects:
+        # String is constructed as follows:
+        # used module (e.g. "mod_sps_") + value name (e.g. "phi")
+        mod_sps_phi: mod_sps calculated values for phi
+        mod_sps_tau1: mod_sps calculated values for tau1
+        mod_sps_tau2: mod_sps calculated values for tau1
+        sim_sps_iLs: simulation results with mod_sps for iLs
+        sim_sps_S11_p_sw:
+
+    :param comment:
+    :param dab:
+    :param directory: Folder where to save the files
+    :param name: String added to the filename. Without file extension. Datetime may prepend the final name.
+    :param timestamp: If the datetime should prepend the final name. default True
+    """
+
+    # Add some descriptive data to the file
+    # Adding a timestamp, it may be useful
+    dab['_timestamp'] = np.asarray(datetime.now().isoformat())
+    # Adding a comment to the file, hopefully a descriptive one
+    if comment:
+        dab['_comment'] = np.asarray(comment)
+
+    # Adding a timestamp to the filename if requested
+    if timestamp:
+        if name:
+            filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + "_" + name
+        else:
+            filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    else:
+        if name:
+            filename = name
+        else:
+            # set some default non-empty filename
+            filename = "dab_dataset"
+
+    if directory:
+        directory = os.path.expanduser(directory)
+        directory = os.path.expandvars(directory)
+        directory = os.path.abspath(directory)
+        if os.path.isdir(directory):
+            file = os.path.join(directory, filename)
+        else:
+            warning("Directory does not exist!")
+            file = os.path.join(filename)
+    else:
+        file = os.path.join(filename)
+
+    # numpy saves everything for us in a handy zip file
+    np.savez_compressed(file=file, **dab)
+
+
+def load_from_file(file: str) -> DAB_Data:
+    """
+    Load everything from the given .npz file.
+    :param file: a .nps filename or file-like object, string, or pathlib.Path
+    :return: two objects with type DAB_Specification and DAB_Results
+    """
+    dab = DAB_Data()
+    # Check for filename extension
+    file_name, file_extension = os.path.splitext(file)
+    if not file_extension:
+        file += '.npz'
+    file = os.path.expanduser(file)
+    file = os.path.expandvars(file)
+    file = os.path.abspath(file)
+    # Open the file and parse the data
+    with np.load(file) as data:
+        spec_keys = None
+        spec_values = None
+        for k, v in data.items():
+            dab[k] = v
+    return dab
+
+
+def save_to_csv(dab: DAB_Data, key=str(), directory=str(), name=str(), timestamp=True):
+    """
+    Save one array with name 'key' out of dab_results to a csv file
+    :param dab:
+    :param key: name of the array in dab_results
+    :param directory:
+    :param name: filename without extension
+    :param timestamp: if the filename should prepended with a timestamp
+    """
+    if timestamp:
+        name = datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + "_" + name
+    filename = os.path.join(directory, name + '_' + key + '.csv')
+
+    if directory:
+        directory = os.path.expanduser(directory)
+        directory = os.path.expandvars(directory)
+        directory = os.path.abspath(directory)
+        if os.path.isdir(directory):
+            file = os.path.join(directory, filename)
+        else:
+            warning("Directory does not exist!")
+            file = os.path.join(filename)
+    else:
+        file = os.path.join(filename)
+
+    comment = key + ' with P: {}, V1: {} and V2: {} steps.'.format(
+        int(dab.P_step),
+        int(dab.V1_step),
+        int(dab.V2_step)
+    )
+
+    # Write the array to disk
+    with open(file, 'w') as outfile:
+        # I'm writing a header here just for the sake of readability
+        # Any line starting with "#" will be ignored by numpy.loadtxt
+        outfile.write('# ' + comment + '\n')
+        outfile.write('# Array shape: {0}\n'.format(dab[key].shape))
+        # x: P, y: V1, z(slices): V2
+        outfile.write('# x: P ({}-{}), y: V1 ({}-{}), z(slices): V2 ({}-{})\n'.format(
+            int(dab.P_min),
+            int(dab.P_max),
+            int(dab.V1_min),
+            int(dab.V1_max),
+            int(dab.V2_min),
+            int(dab.V2_max)
+        ))
+        outfile.write('# z: V2 ' + np.array_str(dab.mesh_V2[:, 0, 0], max_line_width=10000) + '\n')
+        outfile.write('# y: V1 ' + np.array_str(dab.mesh_V1[0, :, 0], max_line_width=10000) + '\n')
+        outfile.write('# x: P ' + np.array_str(dab.mesh_P[0, 0, :], max_line_width=10000) + '\n')
+
+        # Iterating through a ndimensional array produces slices along
+        # the last axis. This is equivalent to data[i,:,:] in this case
+        i = 0
+        for array_slice in dab[key]:
+            # Writing out a break to indicate different slices...
+            outfile.write('# V2 slice {}V\n'.format(
+                (dab.V2_min + i * (dab.V2_max - dab.V2_min) / (dab.V2_step - 1))
+            ))
+            # The formatting string indicates that I'm writing out
+            # the values in left-justified columns 7 characters in width
+            # with 2 decimal places.
+            # np.savetxt(outfile, array_slice, fmt='%-7.2f')
+            np.savetxt(outfile, array_slice, delimiter=';')
+            i += 1
+
+
+# FIXME These classes are deprecated and only for backward compability.
 
 class DAB_Specification(DotMap):
     """
@@ -100,8 +313,8 @@ class DAB_Results(DotMap):
             warning('Value is not an numpy ndarray! Nothing added! Used type: ' + str(type(v)))
 
     def gen_meshes(self, V1_min: float, V1_max: float, V1_step,
-                        V2_min: float, V2_max: float, V2_step,
-                        P_min: float, P_max: float, P_step):
+                   V2_min: float, V2_max: float, V2_step,
+                   P_min: float, P_max: float, P_step):
         """
         Generates the default meshgrids for V1, V2 and P.
         Set the min and max values for your meshgrid with a step size (linspace) of step value.
@@ -125,150 +338,114 @@ class DAB_Results(DotMap):
             self[name_pre + k + name_post] = v
 
 
-# class DAB_Specification:
-# 	"""
-# 	Class to store the DAB specification
-# 	"""
-# 	def __init__(self, V1_nom, V1_min, V1_max, V1_step, V2_nom, V2_min, V2_max, V2_step, P_min, P_max, P_nom, P_step,
-# 				 n, L_s, L_m, fs_nom,
-# 				 fs_min: float = None, fs_max: float = None, fs_step: float = None, L_c: float = None):
-# 		self.V1_nom = V1_nom
-# 		self.V1_min = V1_min
-# 		self.V1_max = V1_max
-# 		self.V1_step = V1_step
-# 		self.V2_nom = V2_nom
-# 		self.V2_min = V2_min
-# 		self.V2_max = V2_max
-# 		self.V2_step = V2_step
-#
-# 		self.P_nom = P_nom
-# 		self.P_min = P_min
-# 		self.P_max = P_max
-# 		self.P_step = P_step
-#
-# 		self.n = n
-# 		self.fs_nom = fs_nom
-# 		self.fs_min = fs_min
-# 		self.fs_max = fs_max
-# 		self.fs_step = fs_step
-#
-# 		self.L_s = L_s
-# 		self.L_m = L_m
-# 		self.L_c = L_c
-#
-# 		# meshgrid for usage in e.g. matrix calculations or contour plot.
-# 		# Link between array indices and x,y,z axes ranges.
-# 		# sparse seems to be fine so far, if it troubles, then change it
-# 		# sparse=False seems to be at least 2 times slower in following calculations!
-# 		self.mesh_V1, self.mesh_V2, self.mesh_P = np.meshgrid(np.linspace(V1_min, V1_max, V1_step),
-# 															  np.linspace(V2_min, V2_max, V2_step),
-# 															  np.linspace(P_min, P_max, P_step), sparse=False)
-#
-#
-# class DAB_Results:
-# 	"""
-# 	Class to store simulation results
-# 	"""
-#
-# 	def __init__(self):
-# 		self.specs= None
-# 		self.mvvp_phi = None
-# 		self.mvvp_tau1 = None
-# 		self.mvvp_tau2 = None
-# 		self.mvvp_iLs = None
-# 		self.mvvp_S11_p_sw = None
-
-
 # ---------- MAIN ----------
 if __name__ == '__main__':
     print("Start of Module Datasets ...")
 
-    # Set the basic DAB Specification
-    # Setting it this way disables tab completion!
-    # Don't use this!
-    dab_test_dict = {'V1_nom':  700,
-                     'V1_min':  600,
-                     'V1_max':  800,
-                     'V1_step': 3,
-                     'V2_nom':  235,
-                     'V2_min':  175,
-                     'V2_max':  295,
-                     'V2_step': 3,
-                     'P_min':   400,
-                     'P_max':   2200,
-                     'P_nom':   2000,
-                     'P_step':  3,
-                     'n':       2.99,
-                     'L_s':     84e-6,
-                     'L_m':     599e-6,
-                     'fs_nom':  200000
-                     }
-    dab_test_dm_no_completion = DAB_Specification(dab_test_dict)
-    # Check Value types
-    for value in dab_test_dm_no_completion.values():
-        print(type(value))
+    # Instantiate the DAB_Data class that holds all our DAB related data
+    dab = DAB_Data()
 
     # Set the basic DAB Specification
-    # Setting it this way enables tab completion!
-    dab_test_dm = DAB_Specification()
-    dab_test_dm.V1_nom = 700
-    dab_test_dm.V1_min = 600
-    dab_test_dm.V1_max = 800
-    dab_test_dm.V1_step = 3
-    dab_test_dm.V2_nom = 235
-    dab_test_dm.V2_min = 175
-    dab_test_dm.V2_max = 295
-    dab_test_dm.V2_step = 3
-    dab_test_dm.P_min = 400
-    dab_test_dm.P_max = 2200
-    dab_test_dm.P_nom = 2000
-    dab_test_dm.P_step = 3
-    dab_test_dm.n = 2.99
-    dab_test_dm.L_s = 84e-6
-    dab_test_dm.L_m = 599e-6
-    dab_test_dm.fs_nom = 200000
+    dab.V1_nom = 700
+    dab.V1_min = 600
+    dab.V1_max = 800
+    dab.V1_step = 3
+    dab.V2_nom = 235
+    dab.V2_min = 175
+    dab.V2_max = 295
+    dab.V2_step = 4
+    dab.P_min = 400
+    dab.P_max = 2200
+    dab.P_nom = 2000
+    dab.P_step = 5
+    dab.n = 2.99
+    dab.L_s = 84e-6
+    dab.L_m = 599e-6
+    dab.fs_nom = 200000
+    debug(dab)
 
-    # Some DotMap access examples
-    print(dab_test_dm.V1_nom)
-    print(dab_test_dm['V2_nom'])
-    print(dab_test_dm)
-    print(dab_test_dm.toDict())
-    dab_test_dm.pprint()
-    dab_test_dm.pprint(pformat='json')
-    # Check Value types
-    for value in dab_test_dm.values():
-        print(type(value))
+    # Test mesh generation
+    dab.gen_meshes()
 
-    # export
-    print("export")
-    spec_keys, spec_values = dab_test_dm.export_to_array()
-    print(spec_keys, spec_values)
-    # import
-    print("import")
-    dab_loaded = DAB_Specification()
-    dab_loaded.import_from_array(spec_keys, spec_values)
-    print(dab_loaded)
+    # Test if single value entries work as expected even so they are ndarrays now
+    step = math.floor((dab.V1_max - dab.V1_min) / 10 + 1)
+    debug(step)
 
-    # a = np.fromiter(dab_test_dm.items(), dtype=dtype, count=len(dab_test_dm))
+    # Test save dab
+    debug(dab)
+    save_to_file(dab, name='test_dab_data', timestamp=False)
 
-# # OLD notation
-# dab_test = ds.DAB_Specification(V1_nom=700,
-# 								V1_min=600,
-# 								V1_max=800,
-# 								V1_step=3,
-# 								V2_nom=235,
-# 								V2_min=175,
-# 								V2_max=295,
-# 								V2_step=3,
-# 								P_min=400,
-# 								P_max=2200,
-# 								P_nom=2000,
-# 								P_step=3,
-# 								n=2.99,
-# 								L_s=84e-6,
-# 								L_m=599e-6,
-# 								fs_nom=200000,
-# 								)
-# print(dab_test.mesh_V1)
-# print(dab_test.mesh_V2)
-# print(dab_test.mesh_P)
+    # Test load dab
+    dab2 = load_from_file('test_dab_data')
+    debug(dab2)
+
+    # Test csv export
+    save_to_csv(dab, 'mesh_V1', name='test_dab_csv', timestamp=False)
+
+    # OLD Deprecated classes:
+
+    # # Set the basic DAB Specification
+    # # Setting it this way disables tab completion!
+    # # Don't use this!
+    # dab_test_dict = {'V1_nom':  700,
+    #                  'V1_min':  600,
+    #                  'V1_max':  800,
+    #                  'V1_step': 3,
+    #                  'V2_nom':  235,
+    #                  'V2_min':  175,
+    #                  'V2_max':  295,
+    #                  'V2_step': 3,
+    #                  'P_min':   400,
+    #                  'P_max':   2200,
+    #                  'P_nom':   2000,
+    #                  'P_step':  3,
+    #                  'n':       2.99,
+    #                  'L_s':     84e-6,
+    #                  'L_m':     599e-6,
+    #                  'fs_nom':  200000
+    #                  }
+    # dab_test_dm_no_completion = DAB_Specification(dab_test_dict)
+    # # Check Value types
+    # for value in dab_test_dm_no_completion.values():
+    #     print(type(value))
+    #
+    # # Set the basic DAB Specification
+    # # Setting it this way enables tab completion!
+    # dab_test_dm = DAB_Specification()
+    # dab_test_dm.V1_nom = 700
+    # dab_test_dm.V1_min = 600
+    # dab_test_dm.V1_max = 800
+    # dab_test_dm.V1_step = 3
+    # dab_test_dm.V2_nom = 235
+    # dab_test_dm.V2_min = 175
+    # dab_test_dm.V2_max = 295
+    # dab_test_dm.V2_step = 3
+    # dab_test_dm.P_min = 400
+    # dab_test_dm.P_max = 2200
+    # dab_test_dm.P_nom = 2000
+    # dab_test_dm.P_step = 3
+    # dab_test_dm.n = 2.99
+    # dab_test_dm.L_s = 84e-6
+    # dab_test_dm.L_m = 599e-6
+    # dab_test_dm.fs_nom = 200000
+    #
+    # # Some DotMap access examples
+    # print(dab_test_dm.V1_nom)
+    # print(dab_test_dm['V2_nom'])
+    # print(dab_test_dm)
+    # print(dab_test_dm.toDict())
+    # dab_test_dm.pprint()
+    # dab_test_dm.pprint(pformat='json')
+    # # Check Value types
+    # for value in dab_test_dm.values():
+    #     print(type(value))
+    #
+    # # export
+    # print("export")
+    # spec_keys, spec_values = dab_test_dm.export_to_array()
+    # print(spec_keys, spec_values)
+    # # import
+    # print("import")
+    # dab_loaded = DAB_Specification()
+    # dab_loaded.import_from_array(spec_keys, spec_values)
+    # print(dab_loaded)
